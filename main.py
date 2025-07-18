6d0eccac3a1f914978089d1c25e48e1ff57d2566
Phat Tran <phat.tran.developer@gmail.com>
2025-07-18 13:43:59 +0700

    Update agent rag

--- main.py
+++ main.py
@@ -7,17 +7,37 @@ from omegaconf import DictConfig
 from src.workflow.agent import RAGAgent
 
 
-async def run_agent(cfg: DictConfig):
+async def run_agent(cfg: DictConfig) -> None:
+    """
+    Run the agent with the given configuration.
+
+    Args:
+        cfg (DictConfig): The configuration for the workflow,
+                          retrieved from the config file.
+
+    Returns:
+        None
+    """
     agent = RAGAgent(cfg.workflow)
     result = await agent.run("Phản ứng oxi hoá khử là gì?")
     print(result)
 
 
 @hydra.main(config_name="config", config_path="src/config", version_base=None)
-def main(cfg: DictConfig):
+def main(cfg: DictConfig) -> None:
+    """
+    Main function to run the agent.
+
+    Args:
+        cfg (DictConfig): The configuration for the workflow,
+                          retrieved from the config file.
+
+    Returns:
+        None
+    """
+    load_dotenv()
     asyncio.run(run_agent(cfg))
 
 
 if __name__ == "__main__":
-    load_dotenv()
     main()
--- src/workflow/agent.py
+++ src/workflow/agent.py
@@ -5,10 +5,28 @@ from omegaconf import DictConfig
 
 
 class RAGAgent:
-    def __init__(self, cfg: DictConfig):
+    """
+    RAGAgent is a class that implements the RAG workflow.
+    It uses the workflow configuration to create an agentic workflow.
+    It then runs the workflow with the given query.
+    It returns the result of the workflow.
+
+    Args:
+        cfg (DictConfig): The configuration for the workflow.
+    """
+
+    def __init__(self, cfg: DictConfig) -> None:
         self.logger = logging.getLogger(__name__)
         self.workflow = hydra.utils.instantiate(cfg)
-        self.logger.info(self.workflow)
 
-    async def run(self, query: str) -> str:
+    async def run(self, query: str) -> any:
+        """
+        Run the workflow with the given query.
+
+        Args:
+            query (str): The query to run the workflow with.
+
+        Returns:
+            any: The result of the workflow.
+        """
         return await self.workflow.run(query)
