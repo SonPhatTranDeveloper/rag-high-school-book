7f67ce8508c8520bb311e713fca2326143639e31
Phat Tran <phat.tran.developer@gmail.com>
2025-07-19 16:05:12 +0700

    Update config

--- start.sh
+++ start.sh
@@ -15,4 +15,4 @@ echo "Waiting 60 seconds before next command..."
 sleep 60
 
 echo "Starting main server..."
-exec uv run gunicorn src.server.wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4
+exec uv run gunicorn src.server.wsgi:app --bind 0.0.0.0:8080 -t 120 --log-level=debug --workers=4
