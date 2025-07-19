0be64ed3d211389c1e3bf25dd7f494447f318184
Phat Tran <phat.tran.developer@gmail.com>
2025-07-19 15:43:07 +0700

    Update script

--- start.sh
+++ start.sh
@@ -11,8 +11,8 @@ sleep 15
 echo "Running llamactl deploy..."
 uv run llamactl deploy src/deployment.yml
 
-echo "Waiting 15 seconds before next command..."
-sleep 15
+echo "Waiting 60 seconds before next command..."
+sleep 60
 
 echo "Starting main server..."
 exec uv run gunicorn src.server.wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4
