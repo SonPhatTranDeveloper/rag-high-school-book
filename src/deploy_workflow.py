11f19193eb4d39614113d9fb60fd1f048bb5e409
Phat Tran <phat.tran.developer@gmail.com>
2025-07-19 11:36:40 +0700

    Update deployment

--- src/deploy_workflow.py
+++ src/deploy_workflow.py
@@ -1,11 +1,4 @@
-import asyncio
-
 from hydra import compose, initialize
-from llama_deploy import (
-    ControlPlaneConfig,
-    WorkflowServiceConfig,
-    deploy_workflow,
-)
 
 from src.workflow.agent import build_workflow
 
@@ -13,22 +6,3 @@ from src.workflow.agent import build_workflow
 with initialize(version_base=None, config_path="config"):
     cfg = compose(config_name="config")
     workflow = build_workflow(cfg)
-
-
-async def main():
-    """
-    Deploy workflow to the control plane
-    """
-    await deploy_workflow(
-        workflow=workflow,
-        workflow_config=WorkflowServiceConfig(
-            service_name="rag-book-workflow",
-            port=8000,
-            host="0.0.0.0",
-        ),
-        control_plane_config=ControlPlaneConfig(),
-    )
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
